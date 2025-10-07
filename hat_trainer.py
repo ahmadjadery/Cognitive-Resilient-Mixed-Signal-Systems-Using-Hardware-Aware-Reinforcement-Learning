3d512ffc817e0c63ef94349f54f9249de04dcd6a
ahmadjadery <ahmad.jadery@net.usj.edu.lb>
2025-10-07 21:48:31 +0300

    Update hat_trainer.py

--- hat_trainer.py
+++ hat_trainer.py
@@ -1,8 +1,10 @@
 import torch
+import torch.nn.functional as F
 import numpy as np
-from ricc_arch import Actor, Critic # Assuming Critic is defined in ricc_arch.py
+from ricc_arch import Actor, Critic
 
 class ReplayBuffer:
+    # ... (هذا الجزء يبقى كما هو) ...
     def __init__(self, max_size=1e6):
         self.storage = []
         self.max_size = int(max_size)
@@ -29,30 +31,35 @@ class ReplayBuffer:
                 np.array(rewards).reshape(-1, 1), np.array(dones).reshape(-1, 1))
 
 class TD3_HAT_Agent:
-    def __init__(self, state_dim, action_dim, lr_actor=3e-4, lr_critic=3e-4, discount=0.99, tau=0.005, use_hat=True):
+    def __init__(self, state_dim, action_dim, policy_freq=2, lr_actor=1e-3, lr_critic=1e-3, discount=0.99, tau=0.005, use_hat=True):
+        # --- Actor Network ---
         self.actor = Actor(state_dim, action_dim)
         self.actor_target = Actor(state_dim, action_dim)
         self.actor_target.load_state_dict(self.actor.state_dict())
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=lr_actor)
 
-        # In a real TD3, there would be two critics
-        self.critic = Critic(state_dim, action_dim) 
-        self.critic_target = Critic(state_dim, action_dim)
-        self.critic_target.load_state_dict(self.critic.state_dict())
-        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=lr_critic)
+        # --- Critic Networks (TD3 uses two critics) ---
+        self.critic_1 = Critic(state_dim, action_dim)
+        self.critic_2 = Critic(state_dim, action_dim)
+        self.critic_target_1 = Critic(state_dim, action_dim)
+        self.critic_target_2 = Critic(state_dim, action_dim)
+        self.critic_target_1.load_state_dict(self.critic_1.state_dict())
+        self.critic_target_2.load_state_dict(self.critic_2.state_dict())
+        self.critic_optimizer = torch.optim.Adam(list(self.critic_1.parameters()) + list(self.critic_2.parameters()), lr=lr_critic)
 
         self.discount = discount
         self.tau = tau
         self.use_hat = use_hat
-    
+        self.policy_freq = policy_freq
+        self.total_it = 0
+
     def select_action(self, state):
         state = torch.FloatTensor(state.reshape(1, -1))
-        # Pass use_hat flag to the forward method
         action = self.actor(state, use_hat=self.use_hat).cpu().data.numpy().flatten()
         return action
 
     def train(self, replay_buffer, batch_size=256):
-        # Sample replay buffer
+        self.total_it += 1
         state, action, next_state, reward, done = replay_buffer.sample(batch_size)
 
         state = torch.FloatTensor(state)
@@ -60,34 +67,41 @@ class TD3_HAT_Agent:
         next_state = torch.FloatTensor(next_state)
         reward = torch.FloatTensor(reward)
         done = torch.FloatTensor(done)
-        
-        # --- Update Critic ---
-        # Clipped Double-Q Learning target
-        with torch.no_grad():
-            next_action = self.actor_target(next_state, use_hat=self.use_hat)
-            target_Q = self.critic_target(next_state, next_action)
-            target_Q = reward + (1 - done) * self.discount * target_Q
-            
-        current_Q = self.critic(state, action)
-        critic_loss = torch.nn.functional.mse_loss(current_Q, target_Q)
 
+        with torch.no_grad():
+            # Select action according to policy and add clipped noise
+            next_action = self.actor_target(next_state, use_hat=self.use_hat)
+            
+            # Compute the target Q value using the "Clipped Double-Q Learning" trick
+            target_Q1 = self.critic_target_1(next_state, next_action)
+            target_Q2 = self.critic_target_2(next_state, next_action)
+            target_Q = torch.min(target_Q1, target_Q2) # Take the minimum of the two critics
+            target_Q = reward + (1 - done) * self.discount * target_Q
+
+        # --- Update Both Critics ---
+        current_Q1 = self.critic_1(state, action)
+        current_Q2 = self.critic_2(state, action)
+        critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+        
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
-        
-        # --- Delayed Actor Update ---
-        actor_loss = -self.critic(state, self.actor(state, use_hat=self.use_hat)).mean()
-        
-        self.actor_optimizer.zero_grad()
-        actor_loss.backward()
-        self.actor_optimizer.step()
 
-        # --- Update Target Networks ---
-        for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
-            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+        # --- Delayed Policy (Actor) Update ---
+        if self.total_it % self.policy_freq == 0:
+            actor_loss = -self.critic_1(state, self.actor(state, use_hat=self.use_hat)).mean()
+            
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
 
-        for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
-            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+            # --- Update Target Networks with soft updates ---
+            for param, target_param in zip(self.critic_1.parameters(), self.critic_target_1.parameters()):
+                target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+            for param, target_param in zip(self.critic_2.parameters(), self.critic_target_2.parameters()):
+                target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+            for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+                target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
             
     def save(self, filename):
         torch.save(self.actor.state_dict(), filename + "_actor.pth")
