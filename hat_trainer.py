30a549baa887df35883ab33fd37a8ca5daefd50d
ahmadjadery <ahmad.jadery@net.usj.edu.lb>
2025-10-08 15:27:41 +0300

    Update hat_trainer.py

--- hat_trainer.py
+++ hat_trainer.py
@@ -3,18 +3,22 @@ import torch.nn.functional as F
 import numpy as np
 from ricc_arch import Actor, Critic
 
+# Define the device globally, to be used by all PyTorch modules
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"INFO: PyTorch is using device: {device}")
+
+# The best version of ReplayBuffer: Pre-allocated NumPy arrays for efficiency.
 class ReplayBuffer:
     def __init__(self, state_dim, action_dim, max_size=int(1e6)):
         self.max_size = max_size
         self.ptr = 0
         self.size = 0
 
-        # Allocate memory once
-        self.state = np.zeros((max_size, state_dim))
-        self.action = np.zeros((max_size, action_dim))
-        self.next_state = np.zeros((max_size, state_dim))
-        self.reward = np.zeros((max_size, 1))
-        self.done = np.zeros((max_size, 1))
+        self.state = np.zeros((max_size, state_dim), dtype=np.float32)
+        self.action = np.zeros((max_size, action_dim), dtype=np.float32)
+        self.next_state = np.zeros((max_size, state_dim), dtype=np.float32)
+        self.reward = np.zeros((max_size, 1), dtype=np.float32)
+        self.done = np.zeros((max_size, 1), dtype=np.float32)
 
     def add(self, state, action, next_state, reward, done):
         self.state[self.ptr] = state
@@ -22,38 +26,36 @@ class ReplayBuffer:
         self.next_state[self.ptr] = next_state
         self.reward[self.ptr] = reward
         self.done[self.ptr] = done
-        
         self.ptr = (self.ptr + 1) % self.max_size
         self.size = min(self.size + 1, self.max_size)
 
     def sample(self, batch_size):
         ind = np.random.randint(0, self.size, size=batch_size)
-
+        # Convert to tensors and move to GPU in one step.
         return (
-            torch.FloatTensor(self.state[ind]),
-            torch.FloatTensor(self.action[ind]),
-            torch.FloatTensor(self.next_state[ind]),
-            torch.FloatTensor(self.reward[ind]),
-            torch.FloatTensor(self.done[ind])
+            torch.FloatTensor(self.state[ind]).to(device),
+            torch.FloatTensor(self.action[ind]).to(device),
+            torch.FloatTensor(self.next_state[ind]).to(device),
+            torch.FloatTensor(self.reward[ind]).to(device),
+            torch.FloatTensor(self.done[ind]).to(device)
         )
 
+# Full, correct TD3 Agent with GPU support
 class TD3_HAT_Agent:
     def __init__(self, state_dim, action_dim, policy_freq=2, lr_actor=1e-3, lr_critic=1e-3, discount=0.99, tau=0.005, use_hat=True):
-        # --- Actor Network ---
-        self.actor = Actor(state_dim, action_dim)
-        self.actor_target = Actor(state_dim, action_dim)
+        self.actor = Actor(state_dim, action_dim).to(device)
+        self.actor_target = Actor(state_dim, action_dim).to(device)
         self.actor_target.load_state_dict(self.actor.state_dict())
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=lr_actor)
 
-        # --- Critic Networks (TD3 uses two critics) ---
-        self.critic_1 = Critic(state_dim, action_dim)
-        self.critic_2 = Critic(state_dim, action_dim)
-        self.critic_target_1 = Critic(state_dim, action_dim)
-        self.critic_target_2 = Critic(state_dim, action_dim)
+        self.critic_1 = Critic(state_dim, action_dim).to(device)
+        self.critic_2 = Critic(state_dim, action_dim).to(device)
+        self.critic_target_1 = Critic(state_dim, action_dim).to(device)
+        self.critic_target_2 = Critic(state_dim, action_dim).to(device)
         self.critic_target_1.load_state_dict(self.critic_1.state_dict())
         self.critic_target_2.load_state_dict(self.critic_2.state_dict())
         self.critic_optimizer = torch.optim.Adam(list(self.critic_1.parameters()) + list(self.critic_2.parameters()), lr=lr_critic)
-
+        
         self.discount = discount
         self.tau = tau
         self.use_hat = use_hat
@@ -61,31 +63,20 @@ class TD3_HAT_Agent:
         self.total_it = 0
 
     def select_action(self, state):
-        state = torch.FloatTensor(state.reshape(1, -1))
-        action = self.actor(state, use_hat=self.use_hat).cpu().data.numpy().flatten()
-        return action
+        state = torch.FloatTensor(state.reshape(1, -1)).to(device)
+        return self.actor(state, use_hat=self.use_hat).cpu().data.numpy().flatten()
 
     def train(self, replay_buffer, batch_size=256):
         self.total_it += 1
         state, action, next_state, reward, done = replay_buffer.sample(batch_size)
 
-        state = torch.FloatTensor(state)
-        action = torch.FloatTensor(action)
-        next_state = torch.FloatTensor(next_state)
-        reward = torch.FloatTensor(reward)
-        done = torch.FloatTensor(done)
-
         with torch.no_grad():
-            # Select action according to policy and add clipped noise
             next_action = self.actor_target(next_state, use_hat=self.use_hat)
-            
-            # Compute the target Q value using the "Clipped Double-Q Learning" trick
             target_Q1 = self.critic_target_1(next_state, next_action)
             target_Q2 = self.critic_target_2(next_state, next_action)
-            target_Q = torch.min(target_Q1, target_Q2) # Take the minimum of the two critics
+            target_Q = torch.min(target_Q1, target_Q2)
             target_Q = reward + (1 - done) * self.discount * target_Q
 
-        # --- Update Both Critics ---
         current_Q1 = self.critic_1(state, action)
         current_Q2 = self.critic_2(state, action)
         critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
@@ -94,15 +85,12 @@ class TD3_HAT_Agent:
         critic_loss.backward()
         self.critic_optimizer.step()
 
-        # --- Delayed Policy (Actor) Update ---
         if self.total_it % self.policy_freq == 0:
             actor_loss = -self.critic_1(state, self.actor(state, use_hat=self.use_hat)).mean()
-            
             self.actor_optimizer.zero_grad()
             actor_loss.backward()
             self.actor_optimizer.step()
 
-            # --- Update Target Networks with soft updates ---
             for param, target_param in zip(self.critic_1.parameters(), self.critic_target_1.parameters()):
                 target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
             for param, target_param in zip(self.critic_2.parameters(), self.critic_target_2.parameters()):
@@ -114,4 +102,5 @@ class TD3_HAT_Agent:
         torch.save(self.actor.state_dict(), filename + "_actor.pth")
         
     def load(self, filename):
-        self.actor.load_state_dict(torch.load(filename + "_actor.pth"))
+        self.actor.load_state_dict(torch.load(filename + "_actor.pth", map_location=device))
+        self.actor_target.load_state_dict(self.actor.state_dict())
